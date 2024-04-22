11990c17eea38eeff1fee936ce2901ab3c9cedcf
7034megatron <emmanuelmabhena0@gmail.com>
2024-04-22 09:46:08 -0700

    initial commit

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,37 +1,37 @@
 #!/usr/bin/python3
-"""For a given employee ID, returns information about
-their TODO list progress"""
 
-import requests
-import sys
+"""
+Python script that, using a REST API, for a given employee ID,
+returns information about his/her TODO list progress.
+"""
+
+from requests import get
+from sys import argv
+
 
 if __name__ == "__main__":
+    response = get('https://jsonplaceholder.typicode.com/todos/')
+    data = response.json()
+    completed = 0
+    total = 0
+    tasks = []
+    response2 = get('https://jsonplaceholder.typicode.com/users')
+    data2 = response2.json()
 
-  userId = sys.argv[1]
-  url = "https://jsonplaceholder.typicode.com/todos?userId={}".format(userId)
+    for i in data2:
+        if i.get('id') == int(argv[1]):
+            employee = i.get('name')
 
-  try:
-    response = requests.get(url)
-    response.raise_for_status()  # Raise an exception for non-2xx status codes
-  except requests.exceptions.RequestException as e:
-    print(f"Error: An error occurred while fetching data: {e}")
-    sys.exit(1)
+    for i in data:
+        if i.get('userId') == int(argv[1]):
+            total += 1
 
-  tasks = response.json()
-  name = None
-  totalTasks = 0
-  completed = 0
+            if i.get('completed') is True:
+                completed += 1
+                tasks.append(i.get('title'))
 
-  for task in tasks:
-    if task.get('userId') == int(userId):
-      totalTasks += 1
-      if task.get('completed'):
-        completed += 1
-      name = name or task.get('name')  # Assign name only once if available
+    print("Employee {} is done with tasks({}/{}):".format(employee, completed,
+                                                          total))
 
-  if not name:
-    print(f"Error: User with ID {userId} not found.")
-    sys.exit(1)
-
-  print('Employee {} is done with tasks({}/{}):'.format(name, completed, totalTasks))
-  print('\n'.join(["\t " + task.get('title') for task in tasks if task.get('completed')]))
+    for i in tasks:
+        print("\t {}".format(i))
